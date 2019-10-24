d4cf2a60a976a672c32cac298be2f3449f60da9d
Isaac <isaac1998188@gmail.com>
2019-10-24 11:40:42 -0700

    se agregan las validaciones para registrar a un usuario

--- app/src/main/java/com/example/usersloginfirebase/RegisterActivity.java
+++ app/src/main/java/com/example/usersloginfirebase/RegisterActivity.java
@@ -3,16 +3,46 @@ package com.example.usersloginfirebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.EditText;
+
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    EditText etEmail, etPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        etEmail = (EditText)findViewById(R.id.tvNewEmail);
+        etPassword = (EditText)findViewById(R.id.tvNewPassword);
     }
 
     public void RegisterUser(){
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+        if(email.isEmpty()){
+            etEmail.setError("Correo requerido");
+            etEmail.requestFocus();
+            return;
+        }
 
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            etEmail.setError("Ingresa una cuenta de correo válido");
+            etEmail.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()){
+            etPassword.setError("Contraseña requerida");
+            etPassword.requestFocus();
+            return;
+        }
+
+        if(password.length()<6){
+            etPassword.setError("La contraseña debe tener al menos 6 caracteres");
+            etPassword.requestFocus();
+            return;
+        }
     }
 }
