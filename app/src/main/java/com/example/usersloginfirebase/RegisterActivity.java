afdb1834b7902133c9a34eb8d474ab32dbd3c428
Isaac <isaac1998188@gmail.com>
2019-10-24 12:53:00 -0700

    se termina la funcionalidad del registro de usuario

--- app/src/main/java/com/example/usersloginfirebase/RegisterActivity.java
+++ app/src/main/java/com/example/usersloginfirebase/RegisterActivity.java
@@ -1,21 +1,48 @@
 package com.example.usersloginfirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
     EditText etEmail, etPassword;
+    Button btnRegister;
+    ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         etEmail = (EditText)findViewById(R.id.tvNewEmail);
         etPassword = (EditText)findViewById(R.id.tvNewPassword);
+        mAuth = FirebaseAuth.getInstance();
+        btnRegister = (Button)findViewById(R.id.btnRegister);
+        progressBar = (ProgressBar)findViewById(R.id.progressBarRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RegisterUser();
+            }
+        });
+
     }
 
     public void RegisterUser(){
@@ -44,5 +71,20 @@ public class RegisterActivity extends AppCompatActivity {
             etPassword.requestFocus();
             return;
         }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressBar.setVisibility(View.GONE);
+                View view = findViewById(R.id.viewRegister);
+                if(task.isSuccessful()){
+                    Snackbar.make(view, "Usuario registrado", Snackbar.LENGTH_SHORT).show();
+                }else{
+                    Snackbar.make(view, "Error al registrarse", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
