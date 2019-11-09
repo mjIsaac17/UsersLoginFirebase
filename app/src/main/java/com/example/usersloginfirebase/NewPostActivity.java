c4990a0df46a717a637f004a47c2c6e170c1dcb7
Isaac <isaac1998188@gmail.com>
2019-11-08 20:33:09 -0800

    método seleccionar y cargar imagen en el image view

--- app/src/main/java/com/example/usersloginfirebase/NewPostActivity.java
+++ app/src/main/java/com/example/usersloginfirebase/NewPostActivity.java
@@ -1,21 +1,76 @@
 package com.example.usersloginfirebase;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+
+import java.io.IOException;
 
 public class NewPostActivity extends AppCompatActivity {
 
+    private static final int CHOOSE_IMAGE = 101;
     Button btnSavePost;
     EditText txtTitle, txtContent;
     ProgressBar progressBar;
+    ImageView imageView;
+    Uri uriImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
+
+        btnSavePost = (Button)findViewById(R.id.btnSavePost);
+        txtContent = (EditText)findViewById(R.id.txtContent);
+        txtTitle = (EditText)findViewById(R.id.txtPostTitle);
+        progressBar = (ProgressBar)findViewById(R.id.progressBarNewPost);
+        imageView = (ImageView)findViewById(R.id.imageView);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chooseImage();
+            }
+        });
+
+        btnSavePost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void chooseImage(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Seleccionar imagen"), CHOOSE_IMAGE);
+
+    }
+
+    @Override
+    //get the selected image
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CHOOSE_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null){
+            uriImage = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uriImage);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
